4dadd2e82b27ff4c35493ff41635ce83e259a1f2
emanuelesguerzo <155169753+emanuelesguerzo@users.noreply.github.com>
2024-11-20 18:54:42 +0100

    doc: cleaned code

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,39 @@
+// BIND
 const $one = document.querySelector.bind(document);
 
+// DATA
 const rowElem = $one(".row")
 const loadingElem = $one(".loading");
 const closeBtn = $one(".close-btn");
 const overlayElem = $one(".overlay");
 const overlayImgElem = $one(".overlay-image");
 
+// FETCHED IMAGES
 let images = [];
 
+// LOADING
+loadingElem.style.display = "block";
+
+// AXIOS FETCH
+axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6")
+    .then(resp => {
+        images = resp.data; 
+        printImages();
+    })
+    .catch(error => {
+        console.error("Errore:", error);
+        loadingElem.style.display = "none";
+        rowElem.innerHTML = `<p class="error">Errore durante il caricamento. Riprova più tardi.</p>`;
+    });
+
+// PRINT IMAGES
 const printImages = () => {
     let result = "";
 
     images.forEach((curImage) => {
         result += `
         <div class="col">
-            <img class="main-img card-image" data-image-id="${curImage.id}" src="${curImage.url}" alt="${curImage.title}">
+            <img class="main-img card-image" src="${curImage.url}" alt="${curImage.title}">
             <p class="card-text">
                 ${curImage.title}
             </p>     
@@ -24,8 +43,8 @@ const printImages = () => {
     rowElem.innerHTML = result;
     loadingElem.style.display = "none"
     
+    // OVERLAY IMAGE SELECTION
     const allImages = document.querySelectorAll(".main-img");
-
     allImages.forEach((img) => {
         img.addEventListener("click", (event) => {
             const imgSrc = event.target.src;
@@ -37,10 +56,7 @@ const printImages = () => {
     })
 }
 
-closeBtn.addEventListener("click", () => {
-    overlayElem.classList.add("hidden");
-})
-
+// CLOSE OVERLAY
 overlayElem.addEventListener("click", (event) => {
     if (event.target === overlayImgElem) {
         event.stopPropagation(); 
@@ -49,18 +65,11 @@ overlayElem.addEventListener("click", (event) => {
     }
 });
 
-loadingElem.style.display = "block";
+// CLOSE BUTTON
+closeBtn.addEventListener("click", () => {
+    overlayElem.classList.add("hidden");
+})
 
-axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6")
-    .then(resp => {
-        images = resp.data; 
-        printImages();
-    })
-    .catch(error => {
-        console.error("Errore:", error);
-        loadingElem.style.display = "none";
-        rowElem.innerHTML = `<p class="error">Errore durante il caricamento. Riprova più tardi.</p>`;
-    });
 
 
 
